d3ae7e258ef838ce51f5f6c7a82fbf55d008a8bb
Wison Ye <wisonye@gmail.com>
2023-03-18 15:54:12 +1300

    [ master ] - Scoreboard definition.

---
+++ src/scoreboard.rs
@@ -0,0 +1,26 @@
+use crate::player::Player;
+use raylib::prelude::Rectangle;
+
+///
+///
+///
+struct Scoreboard {
+    player1: Player,
+    player2: Player,
+}
+
+impl Scoreboard {
+    ///
+    ///
+    ///
+    fn redraw(&self) -> Rectangle {
+        Rectangle::default()
+    }
+
+    ///
+    ///
+    ///
+    fn recalculate_rect(&self) -> Rectangle {
+        Rectangle::default()
+    }
+}
